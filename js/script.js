97786ec27050e785b53dc3289240cda226fe7e27
smanicome <s.manicome@gmail.com>
2021-12-12 22:31:13 +0100

    Smooth scrolling

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('#cinemas-link').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#cinemas").offset().top
+        }, 1000);
+        return false
+    })
+
+    $('#movies-link').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#movies").offset().top
+        }, 1000);
+        return false
+    })
+
+    $('#catering-link').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#catering").offset().top
+        }, 1000);
+        return false
+    })
+
+    $('#contact-link').click(function () {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+        return false
+    })
+});
